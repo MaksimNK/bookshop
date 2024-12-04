bffa116b95419aecf347c8bbe139ff006143844c
maks <maksmozir@gmail.com.com>
2024-12-04 23:15:22 +0300

    Update Logo component and styles

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -4,8 +4,7 @@ import styles from './Logo.module.css'
 export const Logo = (props) => {
     return (
        <div>
-        <img src="image/logo.png" className={styles.image}/>
-        <h3 className={styles.name}>SAOIRSE BOOKS</h3>
+        <img src="/image/logo.png" className={styles.image}/>
        </div>
     );
 }
