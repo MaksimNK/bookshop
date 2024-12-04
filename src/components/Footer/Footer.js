55e028ca733004012ce4d21782a0f1c0a7ecfef6
maks <maksmozir@gmail.com.com>
2024-12-04 23:14:39 +0300

    Update Footer component and styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import styles from './Footer.module.css'
+import { Logo } from "../UI/Logo/Logo";
 
 export const Footer = (props) => {
     return (
        <footer className={styles.footer}>
         <div className={styles.content}>
-            <div>Logo</div>
+            <Logo />
             <h3>Контакты</h3>
         </div>
        </footer>
