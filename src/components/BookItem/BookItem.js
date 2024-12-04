f857ae4b2bdd626d472a951c85fbeb806f5e3d44
maks <maksmozir@gmail.com.com>
2024-12-04 23:14:25 +0300

    Update BookItem component and styles

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,15 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './BookItem.module.css';
+import { useDispatch } from "react-redux";
+import { addToFavorites,  addToBasket} from "../../features/book/bookSlice";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+import { CiBookmark } from "react-icons/ci";
+
 
 export const BookItem = ({ book, onFav, onBuy, isEven }) => {
+
+    const dispatch = useDispatch();
+
+    const handleAddToFavorites = () => {
+        dispatch(addToFavorites(book)); 
+    };
+
+    const handleAddToBasket = () => {
+        dispatch(addToBasket(book));
+    };
+
+
     return (
-        <div className={`${styles.bookitem} ${isEven ? styles.even : styles.odd}`}>
-            <img src={book.image} alt={book.title} className={styles.image} />
-            <h3 className={styles.title}>{book.title}</h3>
-            <h4 className={styles.author}>{book.author}</h4>
-            <p className={styles.price}>${book.price}</p>
-            <button onClick={onFav} className={styles.button}>Fav</button>
-            <button onClick={onBuy} className={styles.button}>Buy</button>
-        </div>
+        <Link to={`/book/${book.id}`} className={`${styles.bookitem} ${isEven ? styles.even : styles.odd}`}>
+            <div>
+                <img src={book.image} alt={book.title} className={styles.image} />
+                <h3 className={styles.title}>{book.title}</h3>
+                <h4 className={styles.author}>{book.author}</h4>
+                <p className={styles.price}>{book.price}</p>
+            </div>
+            <div className={styles.buttoncontainer}>
+                <button 
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleAddToFavorites();
+                    }} 
+                    className={styles.button}
+                >
+                <CiBookmark />
+
+                </button>
+                <button 
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleAddToBasket();
+                    }} 
+                    className={styles.button}
+                >
+                    <MdOutlineShoppingCart />
+                </button>
+            </div>
+        </Link>
     );
 };
