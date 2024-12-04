f46b46b6791a1e31d582be48402f6131c3df0ac1
maks <maksmozir@gmail.com.com>
2024-12-04 23:14:30 +0300

    Update BookList component and styles

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,36 +1,19 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules"; // Import the modules from the correct path
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import styles from "./BookList.module.css";
-import { BookItem } from "../BookItem/BookItem";
+import { BookItem } from "../BookItem/BookItem.js";
+import styles from './BookList.module.css';
 
-export const BookList = ({ books }) => {
+export const BookList = ({ books, onFav, onBuy }) => {
     return (
-        <div className={styles.booklist}>
-            <Swiper
-                modules={[Navigation, Pagination]}
-                navigation
-                pagination={{ clickable: true }}
-                spaceBetween={20}
-                slidesPerView={3}
-                breakpoints={{
-                    640: { slidesPerView: 1 },
-                    768: { slidesPerView: 2 },
-                    1024: { slidesPerView: 3 },
-                }}
-            >
-                {books.map((book, index) => (
-                    <SwiperSlide key={book.id}>
-                        <BookItem
-                            book={book}
-                            isEven={index % 2 === 0}
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+        <div className={styles.bookList}>
+            {books.map((book, index) => (
+                <BookItem 
+                    key={book.id}
+                    book={book}
+                    onFav={() => onFav(book.id)}
+                    onBuy={() => onBuy(book.id)}
+                    isEven={index % 2 === 0}
+                />
+            ))}
         </div>
     );
 };
