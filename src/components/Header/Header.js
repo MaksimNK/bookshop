d027fde40e8dd908f7233cab861d4325a0062888
maks <maksmozir@gmail.com.com>
2024-12-04 23:15:16 +0300

    Update Header component and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./Header.module.css";
+import { CiSearch, CiBookmark } from "react-icons/ci";
+import { PiUserCircleDuotone } from "react-icons/pi";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { Modal } from "../UI/Modal/Modal";
+import { AuthPage } from "../../pages/Auth/AuthPage";
+import { Logo } from "../UI/Logo/Logo";
 
 export const Header = () => {
-    return (
-        <header className={styles.header}>
-            {/* Top navigation */}
-            <nav className={styles.navbar}>
-                <ul className={styles.navbarList}>
-                    <li><Link to="/">Logo</Link></li>
-                    <li><Link to="/catalog">Каталог</Link></li>
-                    <li><Link to="/delivery">Доставка и оплата</Link></li>
-                    <li><Link to="/about">О нас</Link></li>
-                </ul>
-            </nav>
+  const [isProfileModalOpen, setProfileModalOpen] = useState(false);
 
-            {/* Right-side icons */}
-            <nav>
-                <ul className={styles.navbarIconList}>
-                    <li><Link to="/search">Search</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/basket">Basket</Link></li>
-                    <li><Link to="/favorites">Favorites</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const openProfileModal = () => setProfileModalOpen(true);
+  const closeProfileModal = () => setProfileModalOpen(false);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <ul className={styles.navbarList}>
+            <li><Link to="/"><Logo /></Link></li>
+            <li><Link to="/catalog">Каталог</Link></li>
+            <li><Link to="/delivery">Доставка и оплата</Link></li>
+            <li><Link to="/about">О нас</Link></li>
+          </ul>
+        </nav>
+
+        <nav>
+          <ul className={styles.navbarIconList}>
+            <li>
+              <Link to="/search">
+                <CiSearch className={styles.icon} />
+              </Link>
+            </li>
+            <li>
+              {isAuthenticated ? (
+                <Link to="/profile">
+                  <PiUserCircleDuotone className={styles.icon} />
+                </Link>
+              ) : (
+                <button
+                  onClick={openProfileModal}
+                  className={styles.iconButton}
+                >
+                  <PiUserCircleDuotone className={styles.icon} />
+                </button>
+              )}
+            </li>
+            <li>
+              {isAuthenticated ? (
+                <Link to="/basket">
+                  <MdOutlineShoppingCart className={styles.icon} />
+                </Link>
+              ) : (
+                <button
+                  onClick={openProfileModal}
+                  className={styles.iconButton}
+                >
+                  <MdOutlineShoppingCart className={styles.icon} />
+                </button>
+              )}
+            </li>
+            <li>
+              {isAuthenticated ? (
+                <Link to="/favorites">
+                  <CiBookmark className={styles.icon} />
+                </Link>
+              ) : (
+                <button
+                  onClick={openProfileModal}
+                  className={styles.iconButton}
+                >
+                  <CiBookmark className={styles.icon} />
+                </button>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Modal isOpen={isProfileModalOpen} onClose={closeProfileModal}>
+        <AuthPage />
+      </Modal>
+    </>
+  );
 };
