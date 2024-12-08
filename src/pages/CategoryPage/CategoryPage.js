548406691f0fa708eab07e19b7e4fc7eae12c179
MaksimNK <maksmozir@gmail.com>
2024-12-08 21:35:57 +0300

    Improve CategoryPage, DeliveryPage, and PublishingPage layout

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { BookList } from "../../components/BookList/BookList";
@@ -6,18 +6,41 @@ import { fetchBooksCategory } from "../../features/book/bookSlice";
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import styles from './CategoryPage.module.css';
+import { Filter } from "../../components/Filter/Filter";
 
-export const CategoryPage = (props) => {
+export const CategoryPage = () => {
     const { category } = useParams();
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const { books, status, error } = useSelector((state) => state.books);
 
+    const [filteredBooks, setFilteredBooks] = useState(books);
+    const [filter, setFilter] = useState('all');
+
     useEffect(() => {
         dispatch(fetchBooksCategory(category));
-        console.log(books);
     }, [category, dispatch]);
+
+    useEffect(() => {
+        setFilteredBooks(books);
+    }, [books]);
+    const handleFilterChange = (filter) => {
+        setFilter(filter);
+    
+        let filtered = [...books]; 
+
+        if (filter === 'author') {
+            filtered = filtered.filter(book => book.author);
+        } else if (filter === 'title') {
+            filtered = filtered.sort((a, b) => a.title.localeCompare(b.title));
+        } else if (filter === 'price-asc') {
+            filtered = filtered.sort((a, b) => a.price - b.price);
+        }
+    
+        setFilteredBooks(filtered);
+    };
+    
 
     if (status === 'loading') return <p className={styles.loading}>Loading books...</p>;
     if (status === 'failed') navigate('/page-not-found');
@@ -27,8 +50,11 @@ export const CategoryPage = (props) => {
             <Header />
             <div className={styles.categoryPage}>
                 <h1>{category}</h1>
+
+                <Filter onFilterChange={handleFilterChange} /> {/* Add the Filter component */}
+
                 <div className={styles.bookListContainer}>
-                    <BookList books={books} />
+                    <BookList books={filteredBooks} />
                 </div>
             </div>
             <Footer />
--- src/pages/PublishingPage/PublishingPage.js
+++ src/pages/PublishingPage/PublishingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { BookList } from "../../components/BookList/BookList";
@@ -6,6 +6,7 @@ import { fetchBooksPublishing } from "../../features/book/bookSlice";
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import styles from './PublishingPage.module.css';
+import { Filter } from "../../components/Filter/Filter";
 
 export const PublishingPage = (props) => {
     const { publishing } = useParams();
@@ -14,10 +15,33 @@ export const PublishingPage = (props) => {
 
     const { books, status, error } = useSelector((state) => state.books);
 
+    const [filteredBooks, setFilteredBooks] = useState(books);
+    const [filter, setFilter] = useState('all');
+
     useEffect(() => {
         dispatch(fetchBooksPublishing(publishing));
         console.log('Books in PublishingPage:', books);
     }, [publishing, dispatch]);
+
+    useEffect(() => {
+        setFilteredBooks(books);
+    }, [books]);
+
+    const handleFilterChange = (filter) => {
+        setFilter(filter);
+    
+        let filtered = [...books]; 
+
+        if (filter === 'author') {
+            filtered = filtered.filter(book => book.author);
+        } else if (filter === 'title') {
+            filtered = filtered.sort((a, b) => a.title.localeCompare(b.title));
+        } else if (filter === 'price-asc') {
+            filtered = filtered.sort((a, b) => a.price - b.price);
+        }
+    
+        setFilteredBooks(filtered);
+    };
 
     if (status === 'loading') return <p className={styles.loading}>Loading books...</p>;
     if (status === 'failed') navigate('/page-not-found');
@@ -27,8 +51,12 @@ export const PublishingPage = (props) => {
             <Header />
             <div className={styles.publishingPage}>
                 <h1>{publishing} Books</h1>
+
+                {/* Add the Filter component */}
+                <Filter onFilterChange={handleFilterChange} />
+
                 <div className={styles.bookListContainer}>
-                    <BookList books={books} />
+                    <BookList books={filteredBooks} />
                 </div>
             </div>
             <Footer />
