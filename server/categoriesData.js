3f1114fcef12fc937a0ced099c4d5eef67fd13d5
MaksimNK <maksmozir@gmail.com>
2024-12-11 00:34:35 +0300

    Add new image and data files for categories and publishing

---
+++ server/categoriesData.js
@@ -0,0 +1,25 @@
+const categoriesData = [
+    {
+      image: "/image/category/1.png",
+      title: "Художественная литература",
+      category: "художественная литература"
+    },
+    {
+      image: "/image/category/2.png",
+      title: "Биографии и мемуары",
+      category: "Биографии и мемуары"
+    },
+    {
+      image: "/image/category/3.png",
+      title: "Детская литература",
+      category: "Детская литература"
+    },
+    {
+      image: "/image/category/3.png",
+      title: "Рассказ служанки",
+      category: "Детская литература"
+    }
+  ];
+  
+export default categoriesData;
+  
---
+++ server/publishingsData.js
@@ -0,0 +1,25 @@
+const publishingsData = [
+    {
+      title: "Магистраль(Эксмо)",
+      image: "/image/home/header.png",
+      publishing: "magistr"
+    },
+    {
+      title: "Большие книги.иностранка",
+      image: "/image/home/header.png",
+      publishing: "foreignBooks"
+    },
+    {
+      title: "Яркие страницы",
+      image: "/image/home/header.png",
+      publishing: "brightPages"
+    },
+    {
+      title: "Рассказ служанки",
+      image: "/image/home/header.png",
+      publishing: "handmaidsTale"
+    }
+  ];
+  
+export default publishingsData;
+  
